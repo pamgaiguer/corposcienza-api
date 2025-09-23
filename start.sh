6e8c9f77ff6a88005f6cfd998240264ddf0c12cc
Pam Gaiguer <pamella.gaiguer@gmail.com>
2025-09-22 23:43:20 -0300

    adding script to start server

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Garante que está no diretório certo
+cd /opt/corposcienza/corposcienza-api
+
+# Ativa o ambiente virtual
+source venv/bin/activate
+
+# Inicia o Gunicorn com workers
+exec gunicorn core.wsgi:application \
+  --bind 0.0.0.0:3001 \
+  --workers 3
