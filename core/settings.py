a0df3ea4357290e6ab40523e7dc4db2a449d08b6
Pam Gaiguer <pamella.gaiguer@gmail.com>
2025-09-25 23:50:50 -0300

    updating allowd hosts and removing .env temporarly from .gitignore

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i&uqfpi(2+u$^gn7ehp1t0c)m*yg&c^iecpm%zfb6^ley^95-l'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
