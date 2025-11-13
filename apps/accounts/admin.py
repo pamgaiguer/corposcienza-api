6953e6e5159930917403b5bd9d1192a7e3185014
Pam Gaiguer <pamella.gaiguer@gmail.com>
2025-11-12 21:25:54 -0300

    feat: new users, with superusers on default

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,11 +1,67 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # registra o modelo User no admin se não quiser usar o padrão 'auth.User'
-admin.site.unregister(User)  # opcional, pra customizar exibição
+# admin.site.unregister(CustomUser)  # opcional, pra customizar exibição
 
-@admin.register(User)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ("username", "email", "is_staff", "is_active", "last_login")
-    search_fields = ("username", "email")
-    list_filter = ("is_staff", "is_active")
+# @admin.register(CustomUser)
+# class CustomUserAdmin(admin.ModelAdmin):
+#     list_display = ("username", "email", "is_staff", "is_active", "last_login")
+#     search_fields = ("username", "email")
+#     list_filter = ("is_staff", "is_active")
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    list_display = (
+        "username",
+        "email",
+        "nome",
+        "is_active",
+        "is_equipe",
+        "is_staff",
+        "last_login",
+    )
+    search_fields = ("username", "email", "nome")
+    list_filter = ("is_active", "is_equipe", "is_staff")
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Informações pessoais", {"fields": ("nome", "email", "phone")}),
+        (
+            "Permissões",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_equipe",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Datas importantes", {"fields": ("last_login", "date_joined")}),
+    )
+
+    # Campos que aparecem NA TELA DE "ADICIONAR" usuário
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "nome",
+                    "password1",
+                    "password2",
+                    "is_equipe",
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
+    )
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,8 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # exemplo: adicionar campo extra
+    nome = models.CharField("Nome completo", max_length=150, blank=True)
+    is_equipe = models.BooleanField("É da equipe?", default=False)
     phone = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
-        return self.username
+        return self.nome or self.username
